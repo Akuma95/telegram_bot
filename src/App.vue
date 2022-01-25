62ac3e0c46bc48fe955aa73c9755b203a1615a78
Paul25995 <paul-koplin@web.de>
2022-01-25 22:12:59 +0100

    first design

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header />
   <router-view/>
 </template>
 
-<style>
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,14 +29,24 @@
 
 #nav {
   padding: 30px;
+  background-color: #3F50B6;
+  color: #ffffff;
+  display: flex;
+  justify-content: space-between;
+
+  .links {
+    padding-top: 16px;
+  }
+
+  a {
+    font-weight: bold;
+    color: #ffffff;
+
+    &.router-link-exact-active {
+      color: #b9b742;
+    }
+  }
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
